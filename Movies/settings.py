51fb50c8c92b342834e06d013a56c9c9f787352c
Prayas Purusottam <kanha.prayas@gmail.com>
2015-12-04 03:39:19 +0530

    debug false

--- Movies/settings.py
+++ Movies/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '=y8!!7&sajh#lawg8p0u(kpd-&h^ux@+(v-s#(f*w5(y#=j&d9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
