130a9511129dc3ac8bcb1e2d0eb6ea7ed3b77ef6
Prayas Purusottam <kanha.prayas@gmail.com>
2015-12-04 04:18:07 +0530

    wsgi updated

--- Movies/wsgi.py
+++ Movies/wsgi.py
@@ -1,16 +1,4 @@
-"""
-WSGI config for Movies project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
-"""
-
-import os
-
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Movies.settings")
-
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
